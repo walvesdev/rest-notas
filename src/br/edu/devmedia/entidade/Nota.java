30b55bc3ac03c16776c49640733fcaa74a629cd5
willian <willian@T01>
2018-07-04 12:28:14 -0300

    Rest JAX-RS

---
+++ src/br/edu/devmedia/entidade/Nota.java
@@ -0,0 +1,36 @@
+package br.edu.devmedia.entidade;
+
+public  class Nota {
+	
+	private int id;
+	private String titulo;
+	private String descricao;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	@Override
+	public String toString() {
+		return "Nota [id=" + id + ", titulo=" + titulo + ", descricao=" + descricao + "]";
+	}
+	
+	
+	
+	
+}
